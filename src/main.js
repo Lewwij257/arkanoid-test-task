fa60d219f02f9061a3e88fd67583995f39231702
Lewwij257 <Lewwij257@gmail.com>
2026-08-29 01:00:50 +0300

    added platform

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
-import { Application, Assets, Graphics, Sprite } from "pixi.js";
+import { Application, Assets, Text, Container, Graphics, Sprite } from "pixi.js";
 
 (async () => {
    const app = new Application();
 
    await app.init({ background: "#1099bb", resizeTo: window });
+
+   var mousePositionX
+   var mousePositionY
 
    const container = document.getElementById('pixi-container').appendChild(app.canvas);
 
@@ -14,35 +17,56 @@ import { Application, Assets, Graphics, Sprite } from "pixi.js";
    bunnySprite.x = 40;
    bunnySprite.y = 40;
 
-   app.stage.addChild(bunnySprite)
+   //app.stage.addChild(bunnySprite)
 
-   var scale = 1
-   var scaleDirection = 1;
+   const platform = new Graphics();
+   platform.rect(0,0,100,20)
+   platform.fill(0xff0000)
+   platform.stroke(0xfffff)
+   app.stage.addChild(platform)
+   
 
    app.canvas.addEventListener('mousemove', (event) => {
     const react = app.canvas.getBoundingClientRect();
     const x = event.clientX - react.left;
     const y = event.clientY - react.top;
-    bunnySprite.x = x;
-    bunnySprite.y = y;
+    mousePositionX = x;
+    mousePositionY = y;
    })
 
    app.ticker.add((time) => {
-    bunnySprite.rotation += 0.01 * time.deltaTime;
-    scale += 0.01 * scaleDirection * time.deltaTime;
-
-
-    if (scale > 3){
-      scaleDirection = -1
-    }
-    else if (scale < 1){
-      scaleDirection = 1
-    }
-
-
-
-    bunnySprite.scale.set(scale)
+    platform.x = mousePositionX - 50
+    platform.y = app.canvas.getBoundingClientRect().bottom - 50;
    })
+
+  //  var scale = 1
+  //  var scaleDirection = 1;
+
+  //  app.canvas.addEventListener('mousemove', (event) => {
+  //   const react = app.canvas.getBoundingClientRect();
+  //   const x = event.clientX - react.left;
+  //   const y = event.clientY - react.top;
+  //   bunnyContainer.x = x;
+  //   bunnyContainer.y = y;
+  //  })
+
+  //  app.ticker.add((time) => {
+  //   bunnyContainer.rotation += 0.01 * time.deltaTime;
+  //   scale += 0.01 * scaleDirection * time.deltaTime;
+
+
+
+  //   if (scale > 3){
+  //     scaleDirection = -1
+  //   }
+  //   else if (scale < 1){
+  //     scaleDirection = 1
+  //   }
+
+
+
+  //   bunnySprite.scale.set(scale)
+  //  })
   // // Create a new application
   // const app = new Application();
 
